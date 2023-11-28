269d4edba3dc5fad4296973254271d8546d26e4d
Amaury <amaurymechin37@gmail.com>
2023-11-28 20:50:37 +0100

    Restriction inscription + Affichage mdp

--- lib/auth/loggin_page.dart
+++ lib/auth/loggin_page.dart
@@ -14,13 +14,13 @@ class _LoginPageState extends State<LoginPage> {
   // Contrôleurs pour gérer la saisie de l'utilisateur dans les champs d'e-mail et de mot de passe
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
 
   // Méthode de connexion de l'utilisateur
   void _login() async {
     try {
       // Tentative de connexion de l'utilisateur avec l'e-mail et le mot de passe fournis
-      UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
@@ -90,8 +90,23 @@ class _LoginPageState extends State<LoginPage> {
             // Champ de texte pour saisir le mot de passe
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Mot de passe'),
-              obscureText: true, // Mot de passe masqué
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    // Inverse l'état d'obscuration du mot de passe lors du clic sur le bouton
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                  icon: Icon(
+                    // Utilise une icône différente selon l'état d'obscuration
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+              obscureText:
+                  _obscurePassword, // Mot de passe masqué en fonction de l'état
             ),
             const SizedBox(height: 32.0), // Espacement vertical
             // Bouton élevé pour déclencher la connexion
--- lib/auth/signin_page.dart
+++ lib/auth/signin_page.dart
@@ -11,13 +11,64 @@ class _AuthPageState extends State<AuthPage> {
   // Contrôleurs pour gérer la saisie de l'utilisateur dans les champs d'e-mail et de mot de passe
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+
+  // Vérifier si le mot de passe contient au moins une lettre majuscule
+  bool _containsUppercase(String password) {
+    return password.contains(RegExp(r'[A-Z]'));
+  }
+
+  // Vérifier si le mot de passe contient au moins un caractère spécial
+  bool _containsSpecialCharacter(String password) {
+    return password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  }
+
+  bool _containsNumber(String value) {
+    // Vérifie si la chaîne contient au moins un chiffre
+    return value.contains(RegExp(r'\d'));
+  }
 
   // Méthode d'enregistrement de l'utilisateur
   void _register() async {
     try {
+      // Vérifier si l'e-mail est vide
+      if (_emailController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Veuillez saisir une adresse e-mail.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+        return;
+      }
+      // Vérifier si le mot de passe est vide
+      if (_passwordController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Veuillez saisir un mot de passe.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+        return;
+      }
+
+      // Vérifier si le mot de passe répond aux critères
+      if (_passwordController.text.length < 8 ||
+          !_containsUppercase(_passwordController.text) ||
+          !_containsSpecialCharacter(_passwordController.text) ||
+          !_containsNumber(_passwordController.text)) {
+        // Afficher un message d'erreur si le mot de passe est invalide
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+                'Mot de passe invalide. Assurez-vous qu\'il a au moins 8 caractères, une majuscule et un caractère spécial.'),
+            duration: Duration(seconds: 5),
+          ),
+        );
+        return;
+      }
       // Tentative de création d'un nouvel utilisateur avec l'e-mail et le mot de passe fournis
-      UserCredential userCredential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
@@ -37,16 +88,27 @@ class _AuthPageState extends State<AuthPage> {
         ),
       );
     } on FirebaseAuthException catch (e) {
-      // Gestion des exceptions FirebaseAuth, telles qu'un e-mail ou un mot de passe invalide
-      print("Erreur lors de l'inscription : $e");
+      // Gestion des exceptions FirebaseAuth
+      if (e.code == 'email-already-in-use') {
+        // L'e-mail est déjà associé à un compte existant
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Cet e-mail est déjà associé à un compte existant.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      } else {
+        // Autre erreur lors de l'inscription
+        print("Erreur lors de l'inscription : $e");
 
-      // Affichage d'un message d'erreur à l'aide d'un Snackbar
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Erreur lors de l\'inscription'),
-          duration: Duration(seconds: 3),
-        ),
-      );
+        // Affichage d'un message d'erreur à l'aide d'un Snackbar
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Erreur lors de l\'inscription'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      }
     }
   }
 
@@ -83,8 +145,23 @@ class _AuthPageState extends State<AuthPage> {
             // Champ de texte pour saisir le mot de passe
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Mot de passe'),
-              obscureText: true, // Mot de passe masqué
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    // Inverse l'état d'obscuration du mot de passe lors du clic sur le bouton
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                  icon: Icon(
+                    // Utilise une icône différente selon l'état d'obscuration
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+              obscureText:
+                  _obscurePassword, // Mot de passe masqué en fonction de l'état
             ),
             const SizedBox(height: 32.0),
             // Bouton élevé pour déclencher l'enregistrement
