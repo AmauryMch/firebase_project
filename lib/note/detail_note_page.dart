50dfe047291c7a3e518af493be630edbe1b6185e
Amaury <amaurymechin37@gmail.com>
2023-12-02 14:08:04 +0100

    Last modif

--- lib/note/detail_note_page.dart
+++ lib/note/detail_note_page.dart
@@ -32,41 +32,43 @@ class DetailNotePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 12.0),
-            // Conteneur pour afficher le contenu de la note avec une bordure
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                content,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            // Si l'image existe, afficher un conteneur avec l'image et une bordure
-            if (imageUrl.isNotEmpty)
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 12.0),
+              // Conteneur pour afficher le contenu de la note avec une bordure
               Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey),
                   borderRadius: BorderRadius.circular(8.0),
                 ),
                 padding: const EdgeInsets.all(12.0),
-                child: Image.network(
-                  imageUrl,
-                  height: 200.0,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+                child: Text(
+                  content,
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
-          ],
+              const SizedBox(height: 12.0),
+              // Si l'image existe, afficher un conteneur avec l'image et une bordure
+              if (imageUrl.isNotEmpty)
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
